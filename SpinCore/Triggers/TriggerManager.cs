86de2318c4accf67b1ef704e6abbea0aa86fad28
Raoul1808 <raoulthegeek@gmail.com>
2024-05-21 19:07:51 +0200

    Update triggers docs and method signatures

--- SpinCore/Triggers/TriggerManager.cs
+++ SpinCore/Triggers/TriggerManager.cs
@@ -12,19 +12,20 @@ namespace SpinCore.Triggers
         /// Loads the given triggers into the internal trigger manager.
         /// </summary>
         /// <param name="triggers">The list of triggers</param>
-        /// <typeparam name="T">The affected trigger type</typeparam>
+        /// <typeparam name="T">The affected trigger, used as a lookup key</typeparam>
+        /// <exception cref="ArgumentException">Raised if the given array contains nothing</exception>
         public static void LoadTriggers<T>(T[] triggers) where T : ITrigger
         {
-            LoadTriggers(Array.ConvertAll(triggers, t => (ITrigger)t), typeof(T).Name);
+            LoadTriggers(typeof(T).Name, Array.ConvertAll(triggers, t => (ITrigger)t));
         }
 
         /// <summary>
         /// Loads the given triggers into the internal trigger manager.
         /// </summary>
+        /// <param name="key">The internal lookup key</param>
         /// <param name="triggers">The list of triggers</param>
-        /// <param name="key">The key to use internally</param>
         /// <exception cref="ArgumentException">Raised if the given array contains nothing</exception>
-        public static void LoadTriggers(ITrigger[] triggers, string key)
+        public static void LoadTriggers(string key, ITrigger[] triggers)
         {
             if (triggers.Length == 0)
                 throw new ArgumentException("ITrigger array needs to contain triggers");
@@ -52,10 +53,21 @@ namespace SpinCore.Triggers
         /// Fires the given method when a trigger is fired/updates
         /// </summary>
         /// <param name="action">A callback method</param>
-        /// <typeparam name="T">The affected trigger (required)</typeparam>
+        /// <typeparam name="T">The affected trigger, used both as a lookup key and to automatically cast fired triggers into the target trigger type</typeparam>
         public static void RegisterTriggerEvent<T>(TriggerUpdate<T> action) where T : ITrigger
         {
-            RegisterTriggerEvent(typeof(T).Name, (trigger, trackTime) =>
+            RegisterTriggerEvent(typeof(T).Name, action);
+        }
+
+        /// <summary>
+        /// Fires the given method when a trigger is fired/updates
+        /// </summary>
+        /// <param name="key">The internal lookup key</param>
+        /// <param name="action">A callback method</param>
+        /// <typeparam name="T">The affected trigger, used to automatically cast fired triggers into the target trigger type</typeparam>
+        public static void RegisterTriggerEvent<T>(string key, TriggerUpdate<T> action) where T : ITrigger
+        {
+            RegisterTriggerEvent(key, (trigger, trackTime) =>
             {
                 var castTrigger = (T)trigger;
                 action.Invoke(castTrigger, trackTime);
@@ -65,8 +77,8 @@ namespace SpinCore.Triggers
         /// <summary>
         /// Fires the given method when a trigger is fired/updates
         /// </summary>
+        /// <param name="key">The internal lookup key</param>
         /// <param name="action">A callback method</param>
-        /// <param name="key">The key to use internally</param>
         public static void RegisterTriggerEvent(string key, TriggerUpdate action)
         {
             if (string.IsNullOrWhiteSpace(key))
@@ -84,7 +96,7 @@ namespace SpinCore.Triggers
         /// <summary>
         /// Clears triggers for the corresponding trigger store
         /// </summary>
-        /// <typeparam name="T">The affected trigger</typeparam>
+        /// <typeparam name="T">The affected trigger, used as a lookup key</typeparam>
         public static void ClearTriggers<T>() where T : ITrigger
         {
             ClearTriggers(typeof(T).Name);
